1a680fb137e3d1686e335c0b4e85bef9994ec14c
hanamaru7830 <xwsmilexw@gmail.com>
2021-09-19 08:57:53 +0000

    ヘッダーリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  #get 'users/index'
+  #get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  get 'about' => 'homes#show'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
