d023a219b74c0d757b034d8be4bcf947ae0b302f
hanamaru7830 <xwsmilexw@gmail.com>
2021-09-20 05:50:00 +0000

    ユーザーのプロフィール画像変更機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f88a22f4e85e840058d2569cdaa43c22cd7d358eab63dfaded1f447491e8f49266125e9287d1863cdebaccd8de4ce696e6d08666ce50bdb3a77b6ac0080d9cb3'
